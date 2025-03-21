c6a6c16e0d3d484dad99bde0f1e116388fa71f94
Ngozi Aguluka <melissacinta@gmail.com>
2025-03-21 10:17:00 +0100

    fix build error

--- apps/webapp/app/testpage/page.tsx
+++ apps/webapp/app/testpage/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import FeatureCard from '@/components/ui/featureCard';
-import GameCard from '@/components/ui/GameCard';
 import { Users } from 'lucide-react';
 
 const Page = () => {
@@ -29,28 +28,28 @@ const TeamsAndGuildsCard = () => {
   );
 };
 
-const TestGameCard = () => {
-  return (
-    <div className="w-full min-h-screen flex items-center justify-center p-[300px] bg-[black] ">
-      <GameCard
-        rating={9}
-        statusLabel="Active"
-        statusColor="#22C55E"
-        bannerImage="/eternal-legends.svg"
-        gameIcon="/publisherImage.svg"
-        title="Eternal Legends"
-        developer="Blockchain Gaming Inc"
-        description="A revolutionary MMORPG that combines traditional fantasy with blockchain technology."
-        xLink="https://app.onlydust.com/discover"
-        tgLink="https://app.onlydust.com/discover"
-        webLink="https://app.onlydust.com/discover"
-        tags={['MMORPG', 'BLOCKCHAIN', 'FANTASY']}
-        hasWebsiteLink={true}
-        stats={{
-          players: '800,000+',
-          community: '1.5M',
-        }}
-      />
-    </div>
-  );
-};
+// const TestGameCard = () => {
+//   return (
+//     <div className="w-full min-h-screen flex items-center justify-center p-[300px] bg-[black] ">
+//       <GameCard
+//         rating={9}
+//         statusLabel="Active"
+//         statusColor="#22C55E"
+//         bannerImage="/eternal-legends.svg"
+//         gameIcon="/publisherImage.svg"
+//         title="Eternal Legends"
+//         developer="Blockchain Gaming Inc"
+//         description="A revolutionary MMORPG that combines traditional fantasy with blockchain technology."
+//         xLink="https://app.onlydust.com/discover"
+//         tgLink="https://app.onlydust.com/discover"
+//         webLink="https://app.onlydust.com/discover"
+//         tags={['MMORPG', 'BLOCKCHAIN', 'FANTASY']}
+//         hasWebsiteLink={true}
+//         stats={{
+//           players: '800,000+',
+//           community: '1.5M',
+//         }}
+//       />
+//     </div>
+//   );
+// };
