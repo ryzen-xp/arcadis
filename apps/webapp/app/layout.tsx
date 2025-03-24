fa51af40e728e38be2aef93a2514af9076379f85
Joseph Poveda Flores <josephpf12@hotmail.com>
2025-03-24 12:59:42 -0600

    Add header into the project

--- apps/webapp/app/layout.tsx
+++ apps/webapp/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono, Inter } from 'next/font/google'
+import { Header } from '@/components/Header'
 import './globals.css'
 
 const geistSans = Geist({
@@ -26,7 +27,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 	return (
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}>
-				{children}
+				<Header />
+				<div className="pt-16">
+					{children}
+				</div>
 			</body>
 		</html>
 	)
